3f87f37272771a8a2b1d2b17e209aef926f2ad8a
ZakariaElqarch <zakariaelqarch@gmail.com>
2023-02-01 23:42:33 +0100

    fix bootstrap and jquery directory

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,12 @@ use Illuminate\Support\Facades\Route;
 /*Route::get('/', function () {
     return view('welcome');
 });
-
+*/
 
 Route::get('/test', function () {
     return view('test');
 });
-*/
+
 
 
 Route::get('/login', \Auth0\Laravel\Http\Controller\Stateful\Login::class)->name('login');
