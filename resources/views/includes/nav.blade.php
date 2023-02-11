7e54d46f39ce6d9c96aae16f6d88efc0d4a5d5d0
Hamza El Azzouzi <elazzouzi.hamza20@gmail.com>
2023-02-11 20:01:16 +0100

    add navBar

---
+++ resources/views/includes/nav.blade.php
@@ -0,0 +1,28 @@
+<div class=" sidebar" role="navigation">
+    <div class="navbar-collapse">
+        <nav class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-left" id="cbp-spmenu-s1">
+            <ul class="nav" id="side-menu">
+                <li>
+                    <a href="{{ route('index') }}" class="active"><i class="bi bi-house nav_icon"></i>Dashboard</a>
+                </li>
+
+                <li>
+                    <a href="{{ route('index-convention') }}"><i class="bi bi-file-text nav_icon"></i>Convention </a>
+                </li>
+
+                <li>
+                    <a href="{{ route('project') }}"><i class="bi bi-journal-x nav_icon" viewBox="0 0 16 16"></i>Projet
+                    </a>
+                </li>
+
+                <li>
+                    <a href="{{ route('Devision') }}"><i class="bi bi-building nav_icon"></i>Devision </a>
+                </li>
+
+                <li>
+                    <a href="{{ route('Service') }}"><i class="bi bi-house-gear nav_icon"></i>Service </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</div>
